1819aad07d493c3263d3a73b83e1d6d6a4b7ebc9
test4 <martin.j@rxprism.com>
2022-01-19 17:47:19 +0530

    Initial commit

---
+++ app/src/main/java/com/ferring/ownlibrary/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ferring.ownlibrary
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.next).setOnClickListener {
+            startActivity(Intent(this, LibraryActivity::class.java))
+        }
+    }
+}
